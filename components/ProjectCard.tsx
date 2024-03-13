a20ee09d850172c3dcfdaf4909446e367381ab48
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-03-13 17:09:15 +0100

    videos removed from mobile on home, minor changes

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -3,43 +3,50 @@
 import Image from 'next/image';
 import styles from './ProjectCard.module.css';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export default function ProjectCard({ project }: any) {
-  const togglePlay = (event: any) => {
-    const video = event.target as HTMLVideoElement;
-    if (video.paused) {
-      video.play();
-    } else {
-      video.pause();
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setIsDesktop(window.matchMedia('(min-width: 991px)').matches);
     }
-  };
+  }, [isDesktop]);
 
   return (
     <div className={[styles.card, styles[project.size]].join(' ')}>
       <Link
         href={project.url}
-        onMouseEnter={togglePlay}
-        onMouseLeave={togglePlay}
+        onMouseEnter={
+          isDesktop ? (event: any) => event.target.play() : undefined
+        }
+        onMouseLeave={
+          isDesktop ? (event: any) => event.target.pause() : undefined
+        }
       >
         <Image
           src={project.image}
+          className={project.video ? undefined : styles.noVideo}
           alt={project.title}
           fill
           sizes='(max-width: 991px) 50vw, 33vw'
           priority={project.priority ? true : false}
         />
-        <video
-          key={project.id}
-          width='100%'
-          autoPlay={false}
-          muted
-          loop
-          playsInline
-          className={styles.projectVideo}
-        >
-          <source src={project.video} type='video/mp4' />
-          Your browser does not support the video tag.
-        </video>
+        {isDesktop && project.video && (
+          <video
+            key={project.id}
+            width='100%'
+            autoPlay={false}
+            muted
+            loop
+            playsInline
+            className={styles.projectVideo}
+          >
+            <source src={project.video} type='video/mp4' />
+            Your browser does not support the video tag.
+          </video>
+        )}
       </Link>
     </div>
   );
