ca65586467b051272dcffa6097ce97fe4e9ed4d9
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-04-04 13:10:42 +0200

    home page cards changes

--- app/(client-pages)/(home)/page.tsx
+++ app/(client-pages)/(home)/page.tsx
@@ -12,26 +12,26 @@ export default async function Home() {
       type: 'page',
       title: 'About Us',
       description: {
-        __html: 'Digital Dreams.<br>Analog Hearts.<br>Integrated Futures.',
+        __html: 'Digital<br>Dreams.<br>Analog<br>Hearts.<br>Integrated<br>Futures.',
       },
       size: 'medium',
     },
     {
       type: 'page',
       title: 'Meet the Team',
-      description: { __html: 'Meet the People' },
+      description: { __html: 'Meet<br>The People' },
       size: 'square',
     },
     {
       type: 'page',
       title: 'Contact',
-      description: { __html: 'Let&apos;s connect' },
+      description: { __html: 'Let&apos;s<br>Connect' },
       size: 'square',
     },
     {
       type: 'page',
       title: 'Blog',
-      description: { __html: 'The reservoir' },
+      description: { __html: 'The<br>Reservoir' },
       size: 'small',
     },
   ];
