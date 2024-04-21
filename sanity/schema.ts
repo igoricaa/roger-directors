54ebedfc256b3d70c1ccbbb0cb8d81939ffbcf1f
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-04-21 23:12:51 +0200

    reservoir fix

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity';
 import teamMember from './schemas/documents/teamMember';
 import project from './schemas/documents/project';
+import reservoirProject from './schemas/documents/reservoirProject';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [teamMember, project],
+  types: [teamMember, project, reservoirProject],
 };
