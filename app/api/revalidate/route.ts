550c3a3bcadbf12b9371048675920016f1b8081b
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2025-02-11 16:07:19 +0100

    revalidate

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,7 +2,7 @@ import { revalidateTag } from 'next/cache';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const tags = ['project', 'teamMember', 'reservoirProject'];
+  const tags = ['project', 'teamMember', 'reservoirProject', 'pdf'];
 
   for (const tag of tags) {
     revalidateTag(tag);
