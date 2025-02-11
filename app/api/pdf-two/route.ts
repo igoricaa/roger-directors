569db46a5093f977aa5329a6e17e597eae4a2855
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2025-02-11 17:10:56 +0100

    pdf routes

---
+++ app/api/pdf-two/route.ts
@@ -0,0 +1,28 @@
+import { sanityFetch } from '@/utils/sanity/client';
+import { PDF } from '@/utils/types';
+import { redirect } from 'next/navigation';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    const pdf: PDF = await sanityFetch({
+      query: `*[_type == "pdf"][0]{
+      title,
+      'url': pdfFile.asset->url,
+    }`,
+      tags: ['pdf'],
+    });
+
+    console.log(pdf);
+
+    if (!pdf?.url) {
+      return new Response('PDF not found', { status: 404 });
+    }
+
+    // Redirect to the PDF URL
+    return NextResponse.redirect(pdf.url);
+  } catch (error) {
+    console.error(error);
+    return new Response('Error fetching PDF', { status: 500 });
+  }
+}
